9419ed042758e1468854a87c04615ad0a9cc4d00
Nicholas Masso <masonic8@gmail.com>
2013-07-24 18:53:21 -0700

    simplified example

--- Sample2.C
+++ Sample2.C
@@ -6,7 +6,7 @@ class ArduinoController : public Thing
     public:
     
     /** make sure this matches the arduino_name you set for the Greenduino
-        you want to control
+        object you want to interface with
     */
     Str arduino_name = "ardy";
     /** if your arduino is connected to the same computer as this app,
@@ -18,6 +18,7 @@ class ArduinoController : public Thing
     Str output_pool_name;
     Str input_pool_name;
     Text *sensor_value;
+    bool first_heartbeat_heard = false;
 
     ArduinoController () : Thing()
     {  output_pool_name = pool_path + "to-arduino";
@@ -27,7 +28,32 @@ class ArduinoController : public Thing
        sensor_value = new Text ("waiting");
        AppendKid (sensor_value);
     }
+
+    void Blurt (BlurtEvent *e)
+    { if (Utters (e, "victory") )
+        SetPinState (13, true);        
+    }
+
+    void BlurtVanish (BlurtEvent *e)
+    { if (Utters (e, "victory") )
+        SetPinState (13, false);
+    }
     
+    void Metabolize (const Protein &p)
+    { if (HasDescrip (p, arduino_name) && HasDescrip (p, "heartbeat"))
+        { if (! first_heartbeat_heard)
+            { // setup pins
+              SetPinMode (13, "digital-output");
+              SetPinMode (0, "analog-input");
+              first_heartbeat_heard = true;
+            }
+          if (HasIngest (p, "pin-vals"))
+            { Trove <int64> s = TroveFromIngest <int64> (p, "pin-vals");
+              sensor_value -> SetString (ToStr (s . Nth(0)));
+            }
+        }
+    }
+
     // constructs and deposits a protein to turn a digital pin on or off
     void SetPinState (int64 num, bool state)
     { Protein p = ProteinWithDescrip (arduino_name);
@@ -36,56 +62,13 @@ class ArduinoController : public Thing
       AppendIngest (p, "state", state);
       Deposit (p, output_pool_name);
     }
-
+    // constructs and deposits a protein to set a pin's mode
     void SetPinMode (int64 num, Str mode)
     { Protein p = ProteinWithDescrip (arduino_name);
       AppendDescrip (p, "set-pin-mode");
       AppendIngest (p, "pin", num);
       AppendIngest (p, "mode", mode);
-      Deposit (p, pool_path + "to-arduino");
-    }
-
-    void Blurt (BlurtEvent *e)
-    {
-        if (Utters (e, "victory") )
-          SetPinState (13, true);
-        
-        
-        if (Utters (e, "elleshaped") )
-          SetPinState (13, false);
-    }
-    
-    void Metabolize (const Protein &p)
-    {
-        //Arduino is INITIALIZED
-        if (HasDescrip (p, "EInitialized"))
-        {
-            //Set up pin modes
-            SetPinMode (13, "digital-output");
-            SetPinMode (0, "analog-input");
-        }
-        
-        //A DIGITAL pin has changed value
-        if (HasDescrip (p, "EDigitalPinChanged"))
-        {
-            int64 pin = Ingest <int64> (p, "pin");
-            int64 value = Ingest <int64> (p, "value");
-        }
-        
-        //An ANALOG pin has changed value
-        if (HasDescrip (p, "EAnalogPinChanged"))
-        {
-            int64 pin = Ingest <int64> (p, "pin");
-            int64 value = Ingest <int64> (p, "value");
-        }
-
-        if (HasDescrip (p, arduino_name) && HasDescrip (p, "heartbeat"))
-        { if (HasIngest (p, "pin-vals"))
-            { Trove <int64> s = TroveFromIngest <int64> (p, "pin-vals");
-              sensor_value -> SetString (ToStr (s . Nth(0)));
-            }
-
-        }
+      Deposit (p, output_pool_name);
     }
 };
 
