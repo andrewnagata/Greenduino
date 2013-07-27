a57c5c495e1a79d99b41df2cf168613406f8f484
Nicholas Masso <masonic8@gmail.com>
2013-07-27 10:53:57 -0700

    more comments; added Makefile

--- Sample2-worker.C
+++ Sample2-worker.C
@@ -2,6 +2,16 @@
 #include "Greenhouse.h"
 #include "Greenduino.h"
 
+/**
+    run this app on the computer your arduino is connected to
+    make sure the arduino name ("ardy" in this case) is the same
+    as the one defined in Sample2.C
+
+    if this is a different machine than the one running Sample2,
+    you must run /opt/oblong/greenhouse/bin/pool-tcp-server from the
+    command line before running this app
+*/
+
 void Setup ()
 { new Greenduino("ardy", "/dev/tty.usbserial-A9007MYl");
 }
--- Sample2.C
+++ Sample2.C
@@ -1,6 +1,11 @@
 #include "Greenhouse.h"
 #include "Greenduino.h"
 
+/**
+    Run this example with Sample2-worker (on the same machine or a different one)
+    Here you will see how basic I/O with an arduino works via proteins.
+*/
+
 class ArduinoController : public Thing
 {
     public:
@@ -13,63 +18,72 @@ class ArduinoController : public Thing
         leave this string empty
         if its on a different machine, put its tcp address here
         e.g. tcp://Client-Solutions-Test-Macbook-Pro.local/
+        (make sure you have a pool server running on that machine)
     */
-    Str pool_path = "tcp://Client-Solutions-Test-Macbook-Pro.local/";
+    Str pool_path = "";
     Str output_pool_name;
     Str input_pool_name;
     Text *sensor_value;
     bool first_heartbeat_heard = false;
 
     ArduinoController () : Thing()
-    {  output_pool_name = pool_path + "to-arduino";
-       input_pool_name = pool_path + "from-arduino";
-       ParticipateInPool (input_pool_name);
-       ParticipateInPool (output_pool_name);
-       sensor_value = new Text ("waiting");
-       AppendKid (sensor_value);
-    }
+      {  output_pool_name = pool_path + "to-arduino";
+         input_pool_name = pool_path + "from-arduino";
+         ParticipateInPool (input_pool_name);
+         ParticipateInPool (output_pool_name);
+         sensor_value = new Text ("waiting");
+         AppendKid (sensor_value);
+      }
 
     void Blurt (BlurtEvent *e)
-    { if (Utters (e, "victory") )
-        SetPinState (13, true);        
-    }
-
+      { if (Utters (e, "e") || Utters (e, "victory"))
+          SetPinState (13, true);        
+        if (Utters (e, "r") )
+          SetPinState (13, false);
+      }
     void BlurtVanish (BlurtEvent *e)
-    { if (Utters (e, "victory") )
-        SetPinState (13, false);
-    }
+      { if (Utters (e, "victory"))
+          SetPinState (13, false);
+      }
     
     void Metabolize (const Protein &p)
-    { if (HasDescrip (p, arduino_name) && HasDescrip (p, "heartbeat"))
-        { if (! first_heartbeat_heard)
-            { // setup pins
-              SetPinMode (13, "digital-output");
-              SetPinMode (0, "analog-input");
-              first_heartbeat_heard = true;
-            }
-          if (HasIngest (p, "pin-vals"))
-            { Trove <int64> s = TroveFromIngest <int64> (p, "pin-vals");
-              sensor_value -> SetString (ToStr (s . Nth(0)));
-            }
-        }
-    }
+      { if (HasDescrip (p, arduino_name) && HasDescrip (p, "heartbeat"))
+          { if (! first_heartbeat_heard)
+              { // setup pins
+                SetPinMode (13, "digital-output");
+                SetPinMode (0, "analog-input");
+                first_heartbeat_heard = true;
+              }
+            if (HasIngest (p, "pin-vals"))
+              { Trove <int64> s = TroveFromIngest <int64> (p, "pin-vals");
+                sensor_value -> SetString (ToStr (s . Nth(0)));
+              }
+            // set a timer that only goes off if we stop getting heartbeats
+            SetFireTimer (10, 1);
+          }
+      }
+
+    // we've stopped getting heartbeats,
+    // so the arduino must have been disconnected
+    void Fired()
+      { first_heartbeat_heard = false; }
 
     // constructs and deposits a protein to turn a digital pin on or off
     void SetPinState (int64 num, bool state)
-    { Protein p = ProteinWithDescrip (arduino_name);
-      AppendDescrip (p, "set-pin-state");
-      AppendIngest (p, "pin", num);
-      AppendIngest (p, "state", state);
-      Deposit (p, output_pool_name);
-    }
+      { Protein p = ProteinWithDescrip (arduino_name);
+        AppendDescrip (p, "set-pin-state");
+        AppendIngest (p, "pin", num);
+        AppendIngest (p, "state", state);
+        Deposit (p, output_pool_name);
+      }
     // constructs and deposits a protein to set a pin's mode
     void SetPinMode (int64 num, Str mode)
-    { Protein p = ProteinWithDescrip (arduino_name);
-      AppendDescrip (p, "set-pin-mode");
-      AppendIngest (p, "pin", num);
-      AppendIngest (p, "mode", mode);
-      Deposit (p, output_pool_name);
-    }
+      { Protein p = ProteinWithDescrip (arduino_name);
+        AppendDescrip (p, "set-pin-mode");
+        AppendIngest (p, "pin", num);
+        AppendIngest (p, "mode", mode);
+        Deposit (p, output_pool_name);
+      }
 };
 
 void Setup ()
@@ -80,5 +94,4 @@ void Setup ()
     DoNotRegisterForDefaultEvents();
     
     RegisterOneHandBlurt ("victory", VictoryPose);
-    RegisterOneHandBlurt ("elleshaped", LShapePose);
 }
